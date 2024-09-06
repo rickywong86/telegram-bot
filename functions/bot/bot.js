0a37354e6196e679f2357d8ed2799e3202efc51b
Ricky Wong <ricky86uk@gmail.com>
2024-09-06 14:29:33 +0100

    update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,35 +1,23 @@
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const { Telegraf } = require("telegraf")
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start((ctx) => {
-    ctx.reply('Welcome to your Telegram bot! Use /help to see available commands.');
-});
+bot.start(ctx => {
+    console.log("Received /start command")
+    try {
+        return ctx.reply("Hi")
+    } catch (e) {
+        console.error("error in start action:", e)
+        return ctx.reply("Error occured")
+    }
+})
 
-bot.help((ctx) => {
-    ctx.reply('Available commands:\n/newarticle - Create a new article');
-});
-
-bot.command('newarticle', (ctx) => {
-    ctx.reply('Please enter the title of your article:');
-    bot.on('text', async (ctx) => {
-        const title = ctx.message.text;
-        ctx.reply('Please enter the content of your article:');
-        bot.on('text', async (ctx) => {
-            const content = ctx.message.text;
-
-            // Call a function to create the article using the title and content
-            createArticle(ctx, title, content);
-        });
-    });
-});
-
-async function createArticle(ctx, title, content) {
-    // Here you would make a request to the Telegraph API to create the article
-    // For now, let's just log the title and content
-    console.log('Title:', title);
-    console.log('Content:', content);
-
-    ctx.reply('Article created successfully!');
+// AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
+exports.handler = async event => {
+    try {
+        await bot.handleUpdate(JSON.parse(event.body))
+        return { statusCode: 200, body: "" }
+    } catch (e) {
+        console.error("error in handler:", e)
+        return { statusCode: 400, body: "This endpoint is meant for bot and telegram communication" }
+    }
 }
-
-bot.launch();
